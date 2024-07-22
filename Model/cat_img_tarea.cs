9ba47376cffe1ea143bcaf6597b356e33f3e47fa
DanielTrujilloTapia <22300119@uttt.edu.mx>
2024-07-21 18:26:33 -0600

    Add: MODELO cat_img_tarea

---
+++ Model/cat_img_tarea.cs
@@ -0,0 +1,11 @@
+﻿namespace SITEM_API_APP.Model
+{
+    public class cat_img_tarea
+    {
+        public int Id_img { get; set; }
+        public Byte Foto { get; set; }
+        public int Idserviciotarea { get; set; }
+        public int Idfallatarea { get; set; }
+
+    }
+}
